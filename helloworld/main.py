5d8446a6147e92eb952b504d68b037a54b3e9b6b
tolentinomh <oscar.omartin@gmail.com>
2023-02-24 16:09:32 +0100

    Update main.py

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,8 +1,20 @@
-
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def home_view():
-    return "Hello world"
+@app.route('/', methods=['GET', 'POST'])
+def mostrar_imagen():
+    if request.method == 'POST':
+        search_term = request.form['search_term']
+        url = f'https://source.unsplash.com/random/?{search_term}'
+        mas_imagenes = f'Más imágenes de {search_term}'
+    else:
+        url = 'https://source.unsplash.com/random/'
+        mas_imagenes = None
+
+    response = requests.get(url)
+    return render_template('imagen.html', imagen=response.url, mas_imagenes=mas_imagenes, search_term=search_term)
+
+if __name__ == '__main__':
+    app.run(debug=True)
