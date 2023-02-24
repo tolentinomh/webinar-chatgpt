b5e8d996a2c618cf2ec21c96b96bcef18c3f0065
tolentinomh <oscar.omartin@gmail.com>
2023-02-24 16:09:51 +0100

    Add files via upload

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,20 +1,28 @@
-from flask import Flask, render_template, request
-import requests
+from flask import Flask, render_template, send_file, request
+import random
+import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def mostrar_imagen():
-    if request.method == 'POST':
-        search_term = request.form['search_term']
-        url = f'https://source.unsplash.com/random/?{search_term}'
-        mas_imagenes = f'Más imágenes de {search_term}'
+@app.route('/')
+def index():
+    # Obtener el parámetro de búsqueda de la solicitud
+    parametro_busqueda = request.args.get('busqueda')
+    # Crear la URL de la imagen de Unsplash
+    if parametro_busqueda:
+        url_imagen = f'https://source.unsplash.com/random/?{parametro_busqueda}'
     else:
-        url = 'https://source.unsplash.com/random/'
-        mas_imagenes = None
+        url_imagen = 'https://source.unsplash.com/random/'
+    return render_template('index.html', url_imagen=url_imagen)
 
-    response = requests.get(url)
-    return render_template('imagen.html', imagen=response.url, mas_imagenes=mas_imagenes, search_term=search_term)
+@app.route('/descargar-imagen')
+def descargar_imagen():
+    # Obtener la ruta de la imagen seleccionada
+    url_imagen = request.args.get('url_imagen')
+    # Obtener el nombre del archivo
+    nombre_archivo = os.path.basename(url_imagen)
+    # Descargar la imagen
+    return send_file(url_imagen, as_attachment=True, attachment_filename=nombre_archivo)
 
 if __name__ == '__main__':
     app.run(debug=True)
